8d6cdceb27745e49f727bfc4842bee8d947e9d80
Jarrett Kong <jarrettkong@me.com>
2019-09-30 13:58:32 -0600

    Add inital HTTP get request

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("https://www.google.com/")
+	if err != nil {
+		fmt.Printf("Error, %v \n", err)
+		os.Exit(1)
+	}
+	fmt.Println(resp)
+}
